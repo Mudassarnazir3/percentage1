4703783cface787d1c41d26f0ddd1be363d27565
Mudassar <mudassar.nzir320@gmail.com>
2024-03-12 14:23:47 +0500

    first

---
+++ percentage1.js
@@ -0,0 +1,13 @@
+var percentage = 80;
+if (percentage = 80) {
+    console.log("percentage A+ grad");
+}
+else if (percentage = 70) {
+    console.log("percentage = A grade");
+}
+else if (percentage = 60) {
+    console.log("percentage = B grade");
+}
+else if (percentage = 40) {
+    console.log("percentage = Fail");
+}
